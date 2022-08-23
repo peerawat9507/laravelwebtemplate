883027e3adae84f323161aa53ed44f6ade83c541
Peerawat Deshsung <peerawat.d@gmail.com>
2022-08-23 17:05:54 +0700

    updatebackend

--- routes/web.php
+++ routes/web.php
@@ -2,13 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\BackendController;
 
 Route::get('/', [HomeController::class, 'home']);
 Route::get('about', [HomeController::class, 'about']);
 Route::get('service', [HomeController::class, 'service']);
-Route::get('conntact', [HomeController::class, 'conntact']);
+Route::get('contact', [HomeController::class, 'contact']);
 Route::get('login', [HomeController::class, 'login']);
 
-Route::get('product', function(){
-    return "Product Page";
-});
+// Backend Route
+Route::get('backend/dashboard', [BackendController::class, 'dashboard']);
